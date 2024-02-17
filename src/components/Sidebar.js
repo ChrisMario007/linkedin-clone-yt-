7e52ff5fa73c7195078ad4cc678da1ea51bb62bf
ChrisMario007 <crismario60z6@gmail.com>
2024-02-17 11:44:17 +0000

    ss

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,15 @@ import React from 'react'
 import "./css/sidebar.css"
 
 function Sidebar() {
+
+    const recentItem = (topic) => {
+        <div className='sidebar_recentItems'>
+            <span className='sidebar_hash'>#</span>
+            <p> {topic}</p>
+        </div>
+    }
+
+
     return (
         <div className='sidebar'>
             <div className="sidebar_top">
@@ -18,16 +27,21 @@ function Sidebar() {
                     <p>Who viewed you</p>
                     <p className='sidebar_statNumber'>9,862</p>
                 </div>
-                <div className="sidebar_stats">
-                    <div className="sidebar_stat">
-                        <p>Views on Post</p>
-                        <p className='sidebar_statNumber'>17,862</p>
-                    </div>
+
+                <div className="sidebar_stat">
+                    <p>Views on Post</p>
+                    <p className='sidebar_statNumber'>17,862</p>
                 </div>
             </div>
 
+
             <div className="sidebar_bottom">
                 <p>Recent</p>
+                {recentItem('react.js')}
+                {recentItem('programming')}
+                {recentItem('software engineering')}
+                {recentItem('design')}
+                {recentItem('developer')}
 
             </div>
 

a673da339a40b4a97e700e85401c044fbed49037
ChrisMario007 <crismario60z6@gmail.com>
2024-02-18 17:00:55 +0000

    widget news

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -20,7 +20,7 @@ function Widget() {
     </div>
   )
 
-  
+
 
 
   return (
@@ -30,7 +30,14 @@ function Widget() {
         <InfoIcon />
       </div>
 
-      {newArticle("Mario Is at it AhGain", "Top News - 20824 readers")}
+      {newArticle("Mario Is at it AhGain", "Top News - 20,824 readers")}
+      {newArticle("USG to face off with Aston Villa next week", "Sports News - 34,297readers")}
+      {newArticle("Biden blames ‘congressional inaction’ for Ukrainian withdrawal of key town in call with Zelensky ", "Top News - 18,320 readers")}
+      {newArticle("OpenAI will now let you create videos from verbal cues", "Top News - 18,320 readers")}
+      {newArticle("The Alaska Air flight was terrifying. It could have been so much worse", "Top News - 18,320 readers")}
+      {newArticle(" Leading Democrats say their conversations with the vice president have been a surprising and welcome change", "Top News - 18,320 readers")}
+      {newArticle("The ‘Dune: Part Two’ cast’s red carpet looks are out of this world", "Top News - 18,320 readers")}
+      {newArticle("Serving four-year doping ban, former world No. 1 sues health supplement company for damages", "Top News - 18,320 readers")}
     </div>
   )
 }
